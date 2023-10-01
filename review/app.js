c446a5d214336ed0c060109b7412a983b27400f1
Garry Michael Pereira <garrymp12@gmail.com>
2023-09-30 23:30:14 -0500

    0930

---
+++ review/app.js
@@ -0,0 +1,52 @@
+const reviews = [
+    {
+        id: 1,
+        img: "url",
+        name: "Adam Antonio",
+        job: "UX Designer",
+        review: "hehe123-1"
+    },
+    {
+        id: 2,
+        img: "url",
+        name: "Bob Bentley",
+        job: "UI Designer",
+        review: "hehe123-2"
+    },
+    {
+        id: 3,
+        img: "url",
+        name: "Calvin Charles",
+        job: "Web Dev",
+        review: "hehe123-3"
+    },
+    {
+        id: 4,
+        img: "url",
+        name: "Daniel Donaldson",
+        job: "Backend Engineer",
+        review: "hehe123-4"
+    },
+    {
+        id: 5,
+        img: "url",
+        name: "Eric Ertrage",
+        job: "Full-stack Engineer",
+        review: "hehe123-5"
+    },
+    {
+        id: 6,
+        img: "url",
+        name: "Foo Fettuccine",
+        job: "Intern",
+        review: "hehe123-6"
+    },
+    {
+        id: 7,
+        img: "url",
+        name: "Gray Giovanni",
+        job: "Tech Lead",
+        review: "hehe123-7"
+    },
+]
+
